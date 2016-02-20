e75b98f5b3b8bd85b1c40252494b00ee92bdee86
Tony Bailey <baileytj@mail.uc.edu>
2016-02-20 16:37:31 -0500

    Adding an eclipse project onto repo

---
+++ TestProject/test.cpp
@@ -0,0 +1,26 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Feb 20, 2016
+ *      Author: baile_000
+ */
+
+
+
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World";
+	return 0;
+}
