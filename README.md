41643d51e585b6be414e8ff4702587e91feab507
Tony Bailey <baileytj@mail.uc.edu>
2016-02-20 16:28:23 -0500

    testing the touch thingy

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 First Repository
 
 If you are reading this this, then you are reading this.
+If you are reading this, then your are still reading this.
 
---
+++ test.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World";
+	return 0;
+}
